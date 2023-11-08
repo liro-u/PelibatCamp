168081cdf0153c127d38812b851ed61c34440164
liro-u <86537787+liro-u@users.noreply.github.com>
2023-11-08 12:10:46 +0100

    switch image

--- src/Component/Avis/Avis.jsx
+++ src/Component/Avis/Avis.jsx
@@ -50,12 +50,12 @@ const Avis = () => {
         <SingleComment
           name="ADEL, Footballeur professionnel"
           avis="« J'ai contacté JO, dans le cadre de ma préparation avant d'aller en pré saison dans mon club. Il m'a très bien préparé afin d'être près à la reprise. Je suis footballeur professionnel donc j'avais certaines demandes envers lui, et j'ai été très satisfait le travail que l'on a fait était poussé et semblable au sensation d'effort retrouvées en match. » "
-          src="https://cdn.discordapp.com/attachments/1126212462593716359/1171473787527643156/1XSf4_KCoY7cktLvHkrT6gdmlWTY3xYeaE61qBNwg8rVCXHY8oVjLjsO5YaTB2g.png?ex=655ccefb&is=654a59fb&hm=03d2a7f29ee483198209d31a98e9475606e13ca296a731ea7d2f9162f51d8237&"
+          src="https://cdn.discordapp.com/attachments/1126212462593716359/1171474047100518430/1v3fuEvQb5R7E29teSJHD5wedaOWPAxD1Dj2y5Lsv4nTGE88xZXW9Op9K8uH8yIA.png?ex=655ccf39&is=654a5a39&hm=ce4cf1d00978436a9ff190848ec489b75dc37a586fcfbc2e50b75d90f04c4844&"
         />
         <SingleComment
           name="YANIS, Footballeur professionnel"
           avis="« Étant sportif de haut niveaux, j'avais besoin d'une préparation spécifique à mon sport, Jo a pu répondre parfaitement à ses besoins à travers des séances variées. Je le remercie pour son investissement » "
-          src="https://cdn.discordapp.com/attachments/1126212462593716359/1171474047100518430/1v3fuEvQb5R7E29teSJHD5wedaOWPAxD1Dj2y5Lsv4nTGE88xZXW9Op9K8uH8yIA.png?ex=655ccf39&is=654a5a39&hm=ce4cf1d00978436a9ff190848ec489b75dc37a586fcfbc2e50b75d90f04c4844&"
+          src="https://cdn.discordapp.com/attachments/1126212462593716359/1171473787527643156/1XSf4_KCoY7cktLvHkrT6gdmlWTY3xYeaE61qBNwg8rVCXHY8oVjLjsO5YaTB2g.png?ex=655ccefb&is=654a59fb&hm=03d2a7f29ee483198209d31a98e9475606e13ca296a731ea7d2f9162f51d8237&"
         />
       </VBox>
     </VBox>
